726f23574d932696a17a462634994b33d6be90cd
Raphael Kyere Adonteng <68538558+RaphAdonteng@users.noreply.github.com>
2022-11-15 07:50:49 +0000

    New changes in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {BsFacebook, BsInstagram, BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>RAHPY</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#Portfolio">Portfolio</a></li>
+        <li><a href="#Testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com"><BsFacebook/></a>
+        <a href="https://www.instagram.com"><BsInstagram/></a>
+        <a href="https://www.twitter.com"><BsTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy;2022 RAPHY'S Portfolio. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
